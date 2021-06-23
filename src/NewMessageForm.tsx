99f353cd5808088022f226e56878e5c24033c4bd
yceline-yu <cellystuff5@gmail.com>
2021-06-23 10:30:28 -0700

    addedsubmit handler and text input to form

--- src/NewMessageForm.tsx
+++ src/NewMessageForm.tsx
@@ -1,4 +1,6 @@
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 /** NewMessageForm
  * 
@@ -12,12 +14,30 @@ type NewMessageFormProps = {
     username: string
 }
 function NewMessageForm({ username }: NewMessageFormProps) {
+    const initialState = {user: username, messageBody:""};
+    const [formData, setFormData] = useState(initialState);
 
-    return (<Form>
+    function handleChange(evt: { target: { name: any; value: any; }; }){
+        const { name, value } = evt.target;
+        setFormData(curr => ({...curr, [name]: value}));
+    }
+
+    function handleSubmit(evt:React.FormEvent){
+        evt.preventDefault();
+        setFormData(initialState);
+    }
+
+    return (
+    <Form onSubmit={handleSubmit} className="NewMessageForm">
         <Form.Group controlId="newMessageForm.username">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="username" />
+            <Form.Control onChange={handleChange} name="user" value={formData.user} type="text" placeholder="username" />
         </Form.Group>
+        <Form.Group controlId="newMessageForm.messageBody">
+            <Form.Label>Body</Form.Label>
+            <Form.Control as="textarea" onChange={handleChange} name="messageBody" value={formData.messageBody} rows={4} />
+        </Form.Group>
+        <Button type="submit">Submit</Button>
     </Form>)
 }
 
